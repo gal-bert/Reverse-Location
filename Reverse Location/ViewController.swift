56aa87718c407aa37037123744c4843c86babb4e
Gregorius Albert <galbert.djaja@gmail.com>
2023-01-04 19:07:33 +0700

    Initialize core location and get location name from placemark

--- Reverse Location/ViewController.swift
+++ Reverse Location/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    var manager: CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        manager = CLLocationManager()
+        manager?.delegate = self
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.requestWhenInUseAuthorization()
+        manager?.startUpdatingLocation()
+        
+        
+        
     }
+    
+}
 
-
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let first = locations.first else {return}
+        print("\(first.coordinate.latitude) | \(first.coordinate.longitude)")
+        
+        // Async and reactive update
+        lookUpCurrentLocation { placemark in
+            print("\(placemark?.name ?? "No name")")
+        }
+    }
+    
+    func lookUpCurrentLocation(completionHandler: @escaping (CLPlacemark?)
+                               -> Void ) {
+        // Use the last reported location.
+        if let lastLocation = self.manager?.location {
+            let geocoder = CLGeocoder()
+            
+            // Look up the location and pass it to the completion handler
+            geocoder.reverseGeocodeLocation(lastLocation, completionHandler: { (placemarks, error) in
+                if error == nil {
+                    let firstLocation = placemarks?[0]
+                    completionHandler(firstLocation)
+                }
+                else {
+                    // An error occurred during geocoding.
+                    completionHandler(nil)
+                }
+            })
+        }
+        else {
+            // No location was available.
+            completionHandler(nil)
+        }
+    }
+    
 }
 
